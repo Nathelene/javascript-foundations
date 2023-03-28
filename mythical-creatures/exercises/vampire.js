ad3f685f52715ca18e156c042ca3741db5f692c1
Natalie Baker <nathelene76@gmail.com>
2023-03-27 18:13:50 -0600

    Add vampire functions

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,58 @@
 
+function createVampire(name,pet){
+  vampire = {
+    name: name,
+    pet: pet || 'bat',
+    thirsty: true,
+    ouncesDrank: 0
+ }
+ return vampire
+}
+
+function encounterDeliciousVictim(){
+  if (vampire.thirsty === true){
+    return 'I WANT TO SUCK YOUR BLOOD!'
+  } else {
+    return 'No thanks, I am too full.'
+  }
+}
+
+function drink(){
+
+  vampire.ouncesDrank += 10
+  if (vampire.ouncesDrank > 49){
+    vampire.thirsty = false
+    vampire.ouncesDrank = 50
+
+  } 
+
+  return vampire
+}
+
+function inquirePlace(locations,location){
+var response = `No, I have never been to ${location}.`
+  for (i = 0; i < locations.length;i++){
+if (locations[i] === location){
+  var response = `Yes, I have spent some time in New Orleans.`
+   } 
+  }
+  return response
+}
+
+function findBatLovers(vampires){
+  var batLovers = [];
+for (i = 0; i < vampires.length; i++){
+  if(vampires[i].pet === 'bat'){
+    batLovers.push(vampires[i].name)
+  }
+ } 
+ return batLovers
+}
 
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+   createVampire, 
+   drink, 
+  findBatLovers, 
+ encounterDeliciousVictim, 
+  inquirePlace
 }
